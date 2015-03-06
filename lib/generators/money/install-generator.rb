dec219d41cf74ab92014853f587220bc85ad0b42
OleOle7177 <ole_@bk.ru>
2015-03-06 22:26:40 +0600

    Install generator update

--- lib/generators/money/install-generator.rb
+++
@@ -1,8 +0,0 @@
-class InstallGenerator < Rails::Generators::Base  
-      source_root File.expand_path('../templates', __FILE__)  
-             
-      def copy_migrations  
-        copy_file "migrate/create_currency.rb", "db/migrate/"  
-      end  
-        
-end  
