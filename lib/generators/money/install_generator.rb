306f287b6bd668ff266261df91389dabb50a20e6
OleOle7177 <ole_@bk.ru>
2015-03-07 12:58:48 +0600

    Install generator migrations timestamps add

--- lib/generators/money/install_generator.rb
+++ lib/generators/money/install_generator.rb
@@ -1,9 +1,7 @@
 require 'rails/generators/base'
 
 module Money 
-	module Generators
-
-	  class InstallGenerator < Rails::Generators::Base  
+	class InstallGenerator < Rails::Generators::Base  
       	source_root File.expand_path('../templates', __FILE__)  
              
       	def copy_migrations  
@@ -12,5 +10,5 @@ module Money
         
 		end
 	  end
-	end
+	
 end  
