b333a006ce51d503d2aea3faf307b75295517e17
OleOle7177 <ole_@bk.ru>
2015-03-16 17:57:41 +0600

    Money functions fix bugs, create calculated exchage rates on currency create

--- lib/money.rb
+++ lib/money.rb
@@ -2,17 +2,14 @@ module Money
 
   def self.get_base
     code = Currency.find_by_is_base(true).code
-    unless code
-      raise "Не установлена базовая валюта"
-    end
+    raise "Не установлена базовая валюта" unless code   
     code
   end
 
   def self.set_base(sym)	 
     currency = Currency.find_by_code(sym)
-    if currency.nil?
-    	raise "Валюта с указанным кодом не найдена"
-    end
+    
+    raise "Валюта с указанным кодом не найдена" if currency.nil?  
     
     currency.is_base = true
     currency.save
@@ -23,33 +20,33 @@ module Money
   def self.set_currency(fromcode, rate) 
     from = Currency.find_by_code(fromcode)
     to = Currency.find_by_is_base(true)
-    unless to
-      raise "Не установлена базовая валюта" 
-    end
-
-    unless from.id == to.id 
-      CalculatedExchangeRate.create(from_currency_id: from.id, to_currency_id: to.id, rate: rate)
-    end 
+    raise "Не установлена базовая валюта" unless to 
+    
+    CalculatedExchangeRate.create(
+      from_currency_id: from.id, 
+      to_currency_id: to.id, 
+      rate: rate) unless from.id == to.id 
   end
 
   def self.calculate	 
     base = Currency.find_by_is_base(true)
-    unless base
-      raise "Курсы валют не могут быть рассчитаны: не установлена базовая валюта" 
-    end
     
+    raise "Курсы валют не могут быть рассчитаны: не установлена базовая валюта" unless base
+        
     cur_base = base.exchange_rates.last
     
-    unless cur_base
-      raise "Курсы валют не могут быть рассчитаны: нет данных о курсе базовой валюты относительно рубля" 
-	end
-
+    raise "Курсы валют не могут быть рассчитаны: 
+           нет данных о курсе базовой валюты относительно рубля" 
+           unless cur_base
+	  
     @curriencies = Currency.all.includes(:exchange_rates)
     @curriencies.each do |cur|
       cur_from = cur.exchange_rates.last
-      unless cur_from  
-        raise "Курсы валют не могут быть рассчитаны: нет данных о курсе валюты #{cur.code}: #{cur.name} относиельное рубля" 					
-      end
+      
+        raise "Курсы валют не могут быть рассчитаны: 
+               нет данных о курсе валюты #{cur.code}: #{cur.name} относительно рубля"
+               unless cur_from  					
+      
       set_currency(cur.code, (cur_from.rate/cur_base.rate).to_f)
     end		
   end
@@ -60,13 +57,10 @@ module Money
     datetime = options[:datetime] || DateTime.now
 
     to = Currency.find_by_code(tocode)
-    unless from
-      raise "В базе данных нет валюты из которой идёт конвертация"  
-    end
-
-    unless to 
-      raise "В базе данных нет валюты в которую идёт конвертация"  
-	end
+    
+    raise "В базе данных нет валюты из которой идёт конвертация" unless from    
+    raise "В базе данных нет валюты в которую идёт конвертация" unless to 
+	
 
     if from.id == to.id
       1.0
